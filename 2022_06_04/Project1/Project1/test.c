f62679af661408d2abbf537acf03dbb829c58734
ohhhhhwyn <ohhhhhwyn@163.com>
2022-06-04 20:45:44 +0800

    blog code

---
+++ 2022_06_04/Project1/Project1/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//void show() {
+//	printf("hello world!\n");
+//	printf("hello world!\n");
+//	printf("hello world!\n");
+//	printf("hello world!\n");
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	switch (n)
+//	{
+//	case 1:
+//		show();
+//		break;
+//	case 2:
+//		break;
+//	case 3:
+//		break;
+//	default:
+//		break;
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	switch (n)
+//	{
+//	case 1:
+//	{
+//		int a = 0;
+//		int b = 0;
+//		scanf("%d %d", &a, &b);
+//		printf("%d\n", a + b);
+//	}
+//		break;
+//	case 2:
+//		break;
+//	case 3:
+//		break;
+//	default:
+//		break;
+//	}
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	for (int i = 0; i < 1; i++) {
+		printf("hehe");
+	}
+	return 0;
+}
